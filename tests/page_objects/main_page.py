ba68bb1465456a2bdbe160025276d143e2c8592e
moon222 <mon.golebska@gmail.com>
2020-09-20 22:06:35 +0200

    Work in progress

---
+++ tests/page_objects/main_page.py
@@ -0,0 +1,9 @@
+from helpers.funkcje_modul6 import *
+
+main_page_header = 'test-header'
+main_page_content = 'test-content'
+
+
+def content_visible(driver_instance):
+    elem = wait_for_visibility_of_element(driver_instance, main_page_content)
+    return elem.is_displayed()
